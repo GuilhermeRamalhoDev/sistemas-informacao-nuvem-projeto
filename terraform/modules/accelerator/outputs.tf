f59593767893e00f7f83b851f0ecc3b47e3f1d52
Guilherme Ramalho <guilherme.ramalho03@gmail.com>
2026-07-18 17:52:08 +0100

    Limpeza: remover modulo accelerator nao utilizado e corrigir comentario do drill

--- terraform/modules/accelerator/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "static_ip" {
-  description = "IP estático do Global Accelerator (ponto de entrada único)"
-  value       = tolist(aws_globalaccelerator_accelerator.main.ip_sets[0].ip_addresses)[0]
-}
-
-output "dns_name" {
-  value = aws_globalaccelerator_accelerator.main.dns_name
-}
